c90fbfc1750f46ce0a360163b1a50e1ff974309b
changsl <csi2@nate.com>
2015-04-28 12:59:52 +0900

    Signed-off-by: changsl <csi2@nate.com>

--- src/main/java/com/ctart/app/test/code/dao/CodeTestDao.java
+++ src/main/java/com/ctart/app/test/code/dao/CodeTestDao.java
@@ -2,9 +2,10 @@ package com.ctart.app.test.code.dao;
 
 import java.sql.SQLException;
 import java.util.List;
-
+import org.springframework.stereotype.Repository;
 import com.ctart.app.test.code.vo.CodeTestVo;
 
+@Repository
 public interface CodeTestDao {
 	public List<CodeTestVo> selectCodeList() throws SQLException;
 }
